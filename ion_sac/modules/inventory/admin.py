2e20569725d358416f07aacff160b210ba6fe4ed
Kenyi Hancco <kenyihq@gmail.com>
2021-08-18 19:53:10 -0500

    Proyecto iniciado con, se agregó la base de datos

---
+++ ion_sac/modules/inventory/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from modules.inventory.models import *
+
+# Register your models here.
+
+admin.site.register(Cliente)
+admin.site.register(Producto)
+admin.site.register(Pedido)
---
+++ ion_sac/modules/inventory/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+
+class Cliente(models.Model):
+    id_cliente = models.AutoField(primary_key=True)
+    dni = models.CharField(max_length=8)
+    nombre = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    direccion = models.CharField(max_length=255)
+    telefono = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    id_producto = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+    descripcion = models.CharField(max_length=255)
+    precio = models.DecimalField(max_digits=5, decimal_places=2)
+    stock = models.IntegerField()
+    #imagen = models.ImageField(upload_to='productos')
+
+    def __str__(self):
+        return self.nombre
+
+class Pedido(models.Model):
+    id_pedido = models.AutoField(primary_key=True)
+    id_cliente = models.ForeignKey(Cliente, null=False, blank=False, on_delete=models.CASCADE)
+    id_producto = models.ForeignKey(Producto, null=False, blank=False, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.id_cliente.nombre + " " + self.id_producto.nombre
